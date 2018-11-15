522bd87ca66d9b7607d76822f1bd9c2a1f9aa7ca
taiebma <taiebma@free.fr>
2018-11-16 00:24:53 +0100

    Creation device independant pour chaque store

--- modules/StoreSalon/StoreDevice.js
+++ modules/StoreSalon/StoreDevice.js
@@ -13,92 +13,70 @@
 
 ******************************************************************************/
 
-//var request = require("../../request");
-
 // ----------------------------------------------------------------------------
 // --- Class definition, inheritance and setup
 // ----------------------------------------------------------------------------
 
-/*
-function StoreDevice (id, controller, zDeviceId, zInstanceId, zScaleId) {
-    // Call superconstructor first (AutomationModule)
-    StoreDevice.super_.call(this, id, controller, zDeviceId, zInstanceId);
 
-    // Create instance variables
-//    this.deviceType = "virtual";
-//    this.deviceSubType = "luminosity";
-    //this.caps = ["customWidget"];
-
-    this.zCommandClassId = 0x32;
-    this.zSubTreeKey = zScaleId;
-
-    this.deviceType = "probe";
-
-    var self = this;
-}
-
-inherits(StoreDevice, VirtualDevice);
-*/
-
-StoreSalon.prototype.selectStore = function(idStore) {
+StoreSalon.prototype.selectStore = function (idStore) {
 
 	console.log("Selection du store " + idStore);
 
 	var self = this;
 
 	http.request({
-            method: 'GET',
-            url: this.config.urlStoreSelect,
-            data: { }
-        });
+		method: 'GET',
+		url: this.config.urlStoreSelect,
+		data: {}
+	});
 	try {
-               	system( "sleep " + self.config.intervalSelectAttente / 1000 );
-       	} catch(err) {
-               	console.log("Failed to execute script system call sleep : " + err);
-       	}
-	for(i = 0; i < idStore; i++) {
-			console.log("Store " + (i + 1));
+		system("sleep " + self.config.intervalSelectAttente / 1000);
+	} catch (err) {
+		console.log("Failed to execute script system call sleep : " + err);
+	}
+	for (i = 0; i < idStore; i++) {
+		console.log("Store " + (i + 1));
 
-			http.request({
-            			method: 'GET',
-            			url: self.config.urlStoreSelect,
-            			data: { }
-        		});
-                	self.timer = null;
-                	try {
-                        	system(
-                        	"sleep " + self.config.intervalSelectAttente / 1000
-                        	);
-                	} catch(err) {
-                        	console.log("Failed to execute script system call sleep : " + err);
-                	}
+		http.request({
+			method: 'GET',
+			url: self.config.urlStoreSelect,
+			data: {}
+		});
+		self.timer = null;
+		try {
+			system(
+				"sleep " + self.config.intervalSelectAttente / 1000
+			);
+		} catch (err) {
+			console.log("Failed to execute script system call sleep : " + err);
+		}
 
 	}
 	this.currentStore = idStore;
 }
 
-StoreSalon.prototype.selectStoreAll = function() {
+StoreSalon.prototype.selectStoreAll = function () {
 
 	console.log("Retour au store principal " + this.currentStore);
 
 	var self = this;
 
-	for(i = 0; i < this.config.nbChannel + 2 - this.currentStore; i++) {
-			console.log("Store " + (i + 1));
+	for (i = 0; i < this.config.nbChannel + 2 - this.currentStore; i++) {
+		console.log("Store " + (i + 1));
 
-			http.request({
-            			method: 'GET',
-            			url: self.config.urlStoreSelect,
-            			data: { }
-        		});
-                	self.timer = null;
-                	try {
-                        	system(
-                        	"sleep " + self.config.intervalSelectAttente / 1000
-                        	);
-                	} catch(err) {
-                        	console.log("Failed to execute script system call sleep : " + err);
-                	}
+		http.request({
+			method: 'GET',
+			url: self.config.urlStoreSelect,
+			data: {}
+		});
+		self.timer = null;
+		try {
+			system(
+				"sleep " + self.config.intervalSelectAttente / 1000
+			);
+		} catch (err) {
+			console.log("Failed to execute script system call sleep : " + err);
+		}
 
 
 	}
@@ -106,84 +84,84 @@ StoreSalon.prototype.selectStoreAll = function() {
 
 //  Devait servir a mettre a jour le device mais loadJSON ne fonctionne pas a cet endroit
 StoreSalon.prototype.performCommand = function (command, idStore) {
-    console.log("--- StoreDevice.performCommand processing...");
+	console.log("--- StoreDevice.performCommand processing...");
 
-    var handled = true;
-    var self = this;
+	var handled = true;
+	var self = this;
 
-    //  Selection du store
-    this.selectStore(idStore);
+	//  Selection du store
+	this.selectStore(idStore);
 
-    if ("openPartial" === command) {
+	if ("on" === command) {
 
-	console.log("Ouverture partielle du store " + idStore);
-	console.log("Appel de l'URL : " + self.config.urlStoreUp);
+		console.log("Ouverture partielle du store " + idStore);
+		console.log("Appel de l'URL : " + self.config.urlStoreUp);
 
-	//  stop ouverture du store
-	console.log("Stop");
+		//  stop ouverture du store
+		console.log("Stop");
 
-	http.request({
-      		method: 'GET',
-            	url: self.config.urlStoreStop,
-       		data: { }
-      	});
+		http.request({
+			method: 'GET',
+			url: self.config.urlStoreStop,
+			data: {}
+		});
 
-    } else if ("open" === command) {
+	} else if ("open" === command) {
 
-        console.log("Ouverture totale du store " + idStore);
+		console.log("Ouverture totale du store " + idStore);
 
-        //  ouverture du store
-	console.log("Up");
+		//  ouverture du store
+		console.log("Up");
 
-	http.request({
-      		method: 'GET',
-            	url: self.config.urlStoreUp,
-       		data: { }
-      	});
+		http.request({
+			method: 'GET',
+			url: self.config.urlStoreUp,
+			data: {}
+		});
 
 
-    } else if ("close" === command) {
+	} else if ("off" === command) {
 
-        console.log("Fermeture du store " + idStore);
-        
-        //  fermeture du store
-	console.log("Close");
+		console.log("Fermeture du store " + idStore);
 
-	http.request({
-      		method: 'GET',
-            	url: self.config.urlStoreDown,
-       		data: { }
-      	});
+		//  fermeture du store
+		console.log("Close");
 
-    } else if ("stop" === command) {
+		http.request({
+			method: 'GET',
+			url: self.config.urlStoreDown,
+			data: {}
+		});
 
-        console.log("Stop du store " + idStore);
-        
-        //  ouverture du store
-	console.log("Stop");
+	} else if ("stop" === command) {
 
-	http.request({
-      		method: 'GET',
-            	url: self.config.urlStoreStop,
-       		data: { }
-      	});
+		console.log("Stop du store " + idStore);
 
-    } else {
-        handled = false;
-    }
+		//  ouverture du store
+		console.log("Stop");
 
-    //  On attend un peu avant de se remettre au début
-    try {
-       	system(
-               	"sleep " + self.config.intervalSelectAttente *2 / 1000 );
-    } catch(err) {
-        	console.log("Failed to execute script system call sleep : " + err);
-    }
+		http.request({
+			method: 'GET',
+			url: self.config.urlStoreStop,
+			data: {}
+		});
 
-    //  On se repositionne sur le "all channel"
-    self.selectStoreAll();
+	} else {
+		handled = false;
+	}
 
-    return handled ? true : StoreDevice.super_.prototype.performCommand.call(this, command);
+	//  On attend un peu avant de se remettre au début
+	try {
+		system(
+			"sleep " + self.config.intervalSelectAttente * 2 / 1000);
+	} catch (err) {
+		console.log("Failed to execute script system call sleep : " + err);
+	}
+
+	//  On se repositionne sur le "all channel"
+	self.selectStoreAll();
+
+	return handled ? true : StoreDevice.super_.prototype.performCommand.call(this, command);
 }
 
 
--- modules/StoreSalon/index.js
+++ modules/StoreSalon/index.js
@@ -12,53 +12,74 @@ Description: Gestion des stores du salon
 // --- Class definition, inheritance and setup
 // ----------------------------------------------------------------------------
 
-function StoreSalon (id, controller) {
+function StoreSalon(id, controller) {
     // Call superconstructor first (AutomationModule)
     StoreSalon.super_.call(this, id, controller);
 
     var self = this;
 
     // Load abstract StoreDevice device class
-    executeFile(this.moduleBasePath()+"/StoreDevice.js");
+    executeFile(this.moduleBasePath() + "/StoreDevice.js");
 
-    this.vdev = this.controller.devices.create({
-        deviceId: "StoreDevice1",
+    this.vdevTele = this.controller.devices.create({
+        deviceId: "StoreDeviceTele",
         defaults: {
-            deviceType: "switchMultilevel",
+            deviceType: "switchBinary",
             metrics: {
-                title: 'Store Device ' + this.id,
-                icon: ""
+                title: 'Store Device Tele',
+                probeTitle: "Control",
+                icon: "switch"
             }
         },
         overlay: {},
-        handler: function (command, idStore) {
+        handler: function (command) {
 
-		self.performCommand( command, idStore);
+            self.performCommand(command, 1);
         },
         moduleId: this.id
     });
 
-    console.log("Enregistrement de StoreDevice : " + this.vdev.id);
+    console.log("Enregistrement de StoreDevice Tele : " + this.vdevTele.id);
 
-   this.modeNuit = true;
-   this.faitNuit = false;
-   this.currentStore = 0;
-   this.modeVacances = false;
-   this.ciel = "Couvert";
-   this.tempExt = 0;
-   this.modeChaleur  = false;
+    this.vdevTable = this.controller.devices.create({
+        deviceId: "StoreDeviceTable",
+        defaults: {
+            deviceType: "switchBinary",
+            metrics: {
+                title: 'Store Device Table',
+                probeTitle: "Control",
+                icon: "switch"
+           }
+        },
+        overlay: {},
+        handler: function (command) {
+
+            self.performCommand(command, 2);
+        },
+        moduleId: this.id
+    });
+
+    console.log("Enregistrement de StoreDevice : " + this.vdevTable.id);
+
+    this.modeNuit = true;
+    this.faitNuit = false;
+    this.currentStore = 0;
+    this.modeVacances = false;
+    this.ciel = "Couvert";
+    this.tempExt = 0;
+    this.modeChaleur = false;
 
     this.scene = 1;  //  1 = nuit, 2 = journ�ee, 3 = chaleur
 
     var curDate1 = new Date();
-    this.dateDebutJournee = new Date( curDate1.getFullYear(), curDate1.getMonth(), curDate1.getDate(), this.config.heureOuverture, this.config.minuteOuverture, 0);
-    this.dateFinJournee = new Date( curDate1.getFullYear(), curDate1.getMonth(), curDate1.getDate(), this.config.heureFermeture, this.config.minuteFermeture, 0);
+    this.dateDebutJournee = new Date(curDate1.getFullYear(), curDate1.getMonth(), curDate1.getDate(), this.config.heureOuverture, this.config.minuteOuverture, 0);
+    this.dateFinJournee = new Date(curDate1.getFullYear(), curDate1.getMonth(), curDate1.getDate(), this.config.heureFermeture, this.config.minuteFermeture, 0);
     if (curDate1 > this.config.dateDebutJournee && curDate1 < this.config.dateFinJournee) {
-	this.modeNuit = false;
-	this.scene = 2;
+        this.modeNuit = false;
+        this.scene = 2;
     } else {
-	this.modeNuit = true;
-	this.scene = 1; 
+        this.modeNuit = true;
+        this.scene = 1;
     }
     this.tresLumineux = false;
     this.forteChaleur = false;
@@ -78,24 +99,25 @@ StoreSalon.prototype.init = function (config) {
     // Call superclass' init (this will process config argument and so on)
     StoreSalon.super_.prototype.init.call(this, config);
 
-    this.vdev.config = this.config;
+    this.vdevTele.config = this.config;
+    this.vdevTable.config = this.config;
 
     var self = this;
 
     self.controller.emit("cron.addTask", "StoreSalon.Ouverture.poll", {
-	minute: self.config.minuteOuverture,
-	hour: self.config.heureOuverture,
-	weekDay: self.config.weekDay,
-	day: null,
-	month: null
+        minute: self.config.minuteOuverture,
+        hour: self.config.heureOuverture,
+        weekDay: self.config.weekDay,
+        day: null,
+        month: null
     });
 
     self.controller.emit("cron.addTask", "StoreSalon.Fermeture.poll", {
-	minute: self.config.minuteFermeture,
-	hour: self.config.heureFermeture,
-	weekDay: self.config.weekDay,
-	day: null,
-	month: null
+        minute: self.config.minuteFermeture,
+        hour: self.config.heureFermeture,
+        weekDay: self.config.weekDay,
+        day: null,
+        month: null
     });
 
     console.log("StoreSalon : mode nuit " + self.modeNuit);
@@ -105,58 +127,58 @@ StoreSalon.prototype.init = function (config) {
     //  Evenement pour ouverture des stores
     this.controller.on('StoreSalon.Ouverture.poll', function () {
 
-	self.scene = 2;
-    	console.log("StoreSalon : scene " + self.scene);
+        self.scene = 2;
+        console.log("StoreSalon : scene " + self.scene);
 
         //  Arret du cron
-        self.controller.emit("cron.removeTask", "StoreSalon.Ouverture.poll" );
+        self.controller.emit("cron.removeTask", "StoreSalon.Ouverture.poll");
 
-	//  Si il fait jour
-	if (self.faitNuit == false)
-		self.controller.emit("StoreSalon.ouvertureStore") ;
+        //  Si il fait jour
+        if (self.faitNuit == false)
+            self.controller.emit("StoreSalon.ouvertureStore");
 
-	//  Reprogramation pour le lendemain
+        //  Reprogramation pour le lendemain
         self.timer = setTimeout(function () {
 
-                console.log("StoreSalon : Reprogrammation pour le lendemain");
-                self.controller.emit("cron.addTask", "StoreSalon.Ouverture.poll", {
-			minute: self.config.minuteOuverture,
-			hour: self.config.heureOuverture,
-			weekDay: self.config.weekDay,
-                        day: null,
-                        month: null
-                });
-                self.timer = null;
-        }, 60 *1000);
+            console.log("StoreSalon : Reprogrammation pour le lendemain");
+            self.controller.emit("cron.addTask", "StoreSalon.Ouverture.poll", {
+                minute: self.config.minuteOuverture,
+                hour: self.config.heureOuverture,
+                weekDay: self.config.weekDay,
+                day: null,
+                month: null
+            });
+            self.timer = null;
+        }, 60 * 1000);
 
     });
 
     //  Evenement pour fermeture des stores
     this.controller.on('StoreSalon.Fermeture.poll', function () {
 
-	self.scene = 1;
-    	console.log("StoreSalon : scene " + self.scene);
+        self.scene = 1;
+        console.log("StoreSalon : scene " + self.scene);
 
         //  Arret du cron
-        self.controller.emit("cron.removeTask", "StoreSalon.Fermeture.poll" );
+        self.controller.emit("cron.removeTask", "StoreSalon.Fermeture.poll");
 
-	//  Si il fait nuit
-	if (self.faitNuit == true)
-		self.controller.emit("StoreSalon.fermetureStore") ;
+        //  Si il fait nuit
+        if (self.faitNuit == true)
+            self.controller.emit("StoreSalon.fermetureStore");
 
-	//  Reprogramation pour le lendemain
+        //  Reprogramation pour le lendemain
         self.timer = setTimeout(function () {
 
-                console.log("StoreSalon : Reprogrammation pour le lendemain");
-                self.controller.emit("cron.addTask", "StoreSalon.Fermeture.poll", {
-			minute: self.config.minuteFermeture,
-			hour: self.config.heureFermeture,
-			weekDay: self.config.weekDay,
-                        day: null,
-                        month: null
-                });
-                self.timer = null;
-        }, 60 *1000);
+            console.log("StoreSalon : Reprogrammation pour le lendemain");
+            self.controller.emit("cron.addTask", "StoreSalon.Fermeture.poll", {
+                minute: self.config.minuteFermeture,
+                hour: self.config.heureFermeture,
+                weekDay: self.config.weekDay,
+                day: null,
+                month: null
+            });
+            self.timer = null;
+        }, 60 * 1000);
 
     });
 
@@ -164,94 +186,94 @@ StoreSalon.prototype.init = function (config) {
     this.controller.on('StoreSalon.fermetureStore', function (id, name, value) {
         console.log("StoreSalon : Evenement de fermeture des tores");
 
-	//  En mode vacances on touche pas aux stores
-	self.modeVacances = (controller.devices.get("VacancesDevice1").get("metrics:level") == "on")?true:false;
+        //  En mode vacances on touche pas aux stores
+        self.modeVacances = (controller.devices.get("VacancesDevice1").get("metrics:level") == "on") ? true : false;
         console.log("StoreSalon : Fermeture des stores mode vacances = " + self.modeVacances);
-	if (self.modeVacances)
-		return;
+        if (self.modeVacances)
+            return;
         console.log("StoreSalon : Fermeture des stores pas en vacances");
 
-	//  Fermeture des stores
-	if (!self.modeNuit && self.scene == 1) {
-		self.modeNuit = true;
-    		console.log("StoreSalon : Mode nuit , fermeture des stores");
-		self.vdev.performCommand("close", 1);
-                try {
-                    system(
-                       "sleep 10"
-                    );
-                } catch(err) {
-                    console.log("Failed to execute script system call sleep : " + err);
-                }
+        //  Fermeture des stores
+        if (!self.modeNuit && self.scene == 1) {
+            self.modeNuit = true;
+            console.log("StoreSalon : Mode nuit , fermeture des stores");
+            self.vdevTele.performCommand("off");
+            try {
+                system(
+                    "sleep 10"
+                );
+            } catch (err) {
+                console.log("Failed to execute script system call sleep : " + err);
+            }
 
-		self.vdev.performCommand("close", 2);
-	}
+            self.vdevTable.performCommand("off");
+        }
     });
 
     //  Evenement d'ouverture des stores
     this.controller.on('StoreSalon.ouvertureStore', function (id, name, value) {
         console.log("StoreSalon : Evenement d'ouveture des tores");
 
-	//  En mode vacances on ouvre pas
-	self.modeVacances = (controller.devices.get("VacancesDevice1").get("metrics:level") == "on")?true:false;
-	if (self.modeVacances)
-		return;
+        //  En mode vacances on ouvre pas
+        self.modeVacances = (controller.devices.get("VacancesDevice1").get("metrics:level") == "on") ? true : false;
+        if (self.modeVacances)
+            return;
 
-	//  Ouverture des stores
-	if (self.modeNuit && self.scene == 2) {
-		self.modeNuit = false;
-    		console.log("StoreSalon : Mode jour , ouverture des stores");
-		self.vdev.performCommand("openPartial", 1);
-                try {
-                    system(
-                       "sleep 10"
-                    );
-                } catch(err) {
-                    console.log("Failed to execute script system call sleep : " + err);
-                }
-		self.vdev.performCommand("openPartial", 2);
-	}
+        //  Ouverture des stores
+        if (self.modeNuit && self.scene == 2) {
+            self.modeNuit = false;
+            console.log("StoreSalon : Mode jour , ouverture des stores");
+            self.vdevTele.performCommand("on");
+            try {
+                system(
+                    "sleep 10"
+                );
+            } catch (err) {
+                console.log("Failed to execute script system call sleep : " + err);
+            }
+            self.vdevTable.performCommand("on");
+        }
     });
 
     //  Evenement de mise a jour d'evement Device Meteo
     this.controller.on('ModeNuit', function () {
         console.log("StoreSalon : Mode nuit");
-	self.faitNuit = true;
-	//  Fermeture des stores
-	if (self.scene == 1)
-		self.controller.emit("StoreSalon.fermetureStore") ;
+        self.faitNuit = true;
+        //  Fermeture des stores
+        if (self.scene == 1)
+            self.controller.emit("StoreSalon.fermetureStore");
     });
     this.controller.on('ModeJour', function () {
         console.log("StoreSalon : Mode jour");
-	self.faitNuit = false;
-	//  Ouverture des stores
-	if (self.scene == 2)
-		self.controller.emit("StoreSalon.ouvertureStore") ;
+        self.faitNuit = false;
+        //  Ouverture des stores
+        if (self.scene == 2)
+            self.controller.emit("StoreSalon.ouvertureStore");
     });
 
     // Verification des conditions meteo
     this.controller.on('MeteoDevice1.meteoUpdated', function () {
-	self.ciel = controller.devices.get("MeteoDevice1").get("metrics:forecast_summary");
-	self.tempExt = controller.devices.get("MeteoDevice1").get("metrics:tempExt");
+        self.ciel = controller.devices.get("MeteoDevice1").get("metrics:forecast_summary");
+        self.tempExt = controller.devices.get("MeteoDevice1").get("metrics:tempExt");
 
-	var curDate1 = new Date();
+        var curDate1 = new Date();
 
-	//  Verification condition meteo
-	console.log("StoreSalon : Ciel " + self.ciel + " Temperature exterieur " + self.tempExt + " heure " + curDate1.getHours());
-	if (self.ciel == "Clear" && self.tempExt > 24.0 && curDate1.getHours()  >16 ) {
-		if (!self.modeChaleur) { //  Pas encore baiss�e le store
-    			console.log("StoreSalon : Forte chaleur et forte luminosite faux fermer le store");
+        //  Verification condition meteo
+        console.log("StoreSalon : Ciel " + self.ciel + " Temperature exterieur " + self.tempExt + " heure " + curDate1.getHours());
+        if (self.ciel == "Clear" && self.tempExt > 24.0 && curDate1.getHours() > 16) {
+            if (!self.modeChaleur) { //  Pas encore baiss�e le store
+                console.log("StoreSalon : Forte chaleur et forte luminosite faux fermer le store");
 
-			//  En mode vacances on touche pas aux stores
-			self.modeVacances = (controller.devices.get("VacancesDevice1").get("metrics:level") == "on")?true:false;
-			if (self.modeVacances)
-				return;
+                //  En mode vacances on touche pas aux stores
+                self.modeVacances = (controller.devices.get("VacancesDevice1").get("metrics:level") == "on") ? true : false;
+                if (self.modeVacances)
+                    return;
 
-			self.vdev.performCommand("close", 2);
-			self.modeChaleur = true;
-		}
-	} else
-		self.modeChaleur = false;
+                self.vdevTable.performCommand("off");
+                self.modeChaleur = true;
+            }
+        } else
+            self.modeChaleur = false;
     });
 
 };
